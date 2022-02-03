b7af0a4773cc7c9d28a7529870df68e00769ba88
Krzysztof Ziółkowski <ziolko@Krzysztofs-MBP.home>
2022-02-03 08:28:41 +0100

    Update GET /api/<url>

--- src/main/scala/Service.scala
+++ src/main/scala/Service.scala
@@ -7,7 +7,6 @@ import org.http4s._
 import org.http4s.circe.CirceEntityDecoder._
 import org.http4s.circe.CirceEntityEncoder._
 import org.http4s.dsl.Http4sDsl
-import org.http4s.implicits.http4sLiteralsSyntax
 
 object Service extends Http4sDsl[IO] with LazyLogging {
 
@@ -40,15 +39,32 @@ object Service extends Http4sDsl[IO] with LazyLogging {
         case GET -> Root / "api" / "stats" => Ok(directories)
 
         // to improve
-        case GET -> Root / "api" / URLVar(shortURL) =>
-          for {
-            maybeDir <- directories
-              .map(_.filter(_.shortURL.get == shortURL))
-              .option
-            dir = maybeDir.get.head.next
-            _ <- ref.tryUpdate(_.filterNot(_.fullURL == dir.fullURL) :+ dir)
-            response <- Ok(dir)
-          } yield response
+        case GET -> Root / "api" / URLVar(url) =>
+          directories.map(_.exists(_.shortURL.get == url)).flatMap {
+            case false =>
+              // fullURL
+              directories.map(_.exists(_.fullURL == url)).flatMap {
+                case false => BadRequest()
+                case _ =>
+                  for {
+                    maybeDir <- directories
+                      .map(_.filter(_.fullURL == url))
+                      .option
+                    dir = maybeDir.get.head.next
+                    response <- Ok(dir)
+                  } yield response
+              }
+            case _ =>
+              // shortURL
+              for {
+                maybeDir <- directories
+                  .map(_.filter(_.shortURL.get == url))
+                  .option
+                dir = maybeDir.get.head.next
+                _ <- ref.tryUpdate(_.filterNot(_.fullURL == dir.fullURL) :+ dir)
+                response <- Ok(dir)
+              } yield response
+          }
 
         case GET -> Root / "api" / "stats" / URLVar(fullURL) =>
           directories.map(_.filter(_.fullURL == fullURL)).map {
